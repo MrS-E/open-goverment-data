2044db7b85e1ec25d7274d47323c48c3a3ddd289
MrS-E <simeon.stix@gmx.ch>
2023-05-21 20:01:08 +0200

    working add reading the fetched data

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import {fetch} from "next/dist/compiled/@edge-runtime/primitives/fetch";
-import Map from '@/components/Tg'
-import {useEffect} from "react";
+import Map from '@/components/ThurgauMap'
+import {useEffect, useState} from "react";
 import styled from "styled-components";
 
 export const Tooltip = styled.p`
@@ -13,10 +13,15 @@ export const Tooltip = styled.p`
   pointer-events: none;
 `;
 
-export default function Home({age}) {
-    console.log(age)
+export default function Home(props) {
+    const [energy, changeEnergy] = useState(props.energy)
     useEffect(()=>{
         for(let doc of document.querySelectorAll('path')){
+            let result = energy.filter(obj => {
+                return (obj.bfs_nr_gemeinde == doc.id && obj.jahr==2020);
+            }).sort((a,b) => (a.jahr > b.jahr) ? -1 : ((b.jahr > a.jahr) ? 1 : 0))[0]
+            console.log(result)
+            changeEnergy(oldArray => [...oldArray, result])
             doc.addEventListener('click', (e)=>{
                 let d = document.querySelector('p')
                 d.removeAttribute("style")
@@ -32,9 +37,9 @@ export default function Home({age}) {
                 <title>Open Government Data</title>
                 <meta name="viewport" content="width=device-width, initial-scale=1"/>
             </Head>
-                <main>
+            <main>
                 <Map/>
-                    <Tooltip style={"display:none"}>sadf</Tooltip>
+                <Tooltip style={"display:none"}>sadf</Tooltip>
             </main>
         </>
     )
@@ -45,6 +50,6 @@ export async function getStaticProps({params}) {
     const data = await req.json();
 
     return {
-        props: {age: data},
+        props: {energy: data},
     }
 }
