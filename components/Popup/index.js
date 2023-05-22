08c741e3c17dff0ed9470447a0e8947af93feeb1
MrS-E <simeon.stix@gmx.ch>
2023-05-22 15:48:38 +0200

    adding tailwind and removing bootstrap

--- components/Popup/index.js
+++ components/Popup/index.js
@@ -7,7 +7,16 @@ function Popup(props) { //simple popup
             <div className={styles.popup}>
                 <div className={styles.popup_inner}>
                     <div>
-                        <button className="close-btn" onClick={() => {props.changeTrigger(false)}}>close</button> {/*to close popup*/}
+                        <button type="button"
+                                onClick={() => {props.changeTrigger(false)}}
+                                className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                            <span className="sr-only">Close menu</span>
+                            <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                 stroke="currentColor" aria-hidden="true">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                      d="M6 18L18 6M6 6l12 12"/>
+                            </svg>
+                        </button>
                     </div>
                     <div className="container">
                         {props.children} {/*shows all children -> can be used everywhere*/}
