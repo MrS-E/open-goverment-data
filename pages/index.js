d385a20ad86afc98ba6dba28f1f0831dc2cb840f
MrS-E <simeon.stix@gmx.ch>
2023-05-22 16:17:20 +0200

    start with displaying data in map

--- pages/index.js
+++ pages/index.js
@@ -9,17 +9,20 @@ import { PrismaClient } from '@prisma/client'
 //const prisma = new PrismaClient()
 
 export default function Home(props) {
-    const [energy, changeEnergy] = useState(props.energy)
+    const [energy, changeEnergy] = useState([])
     const [popup, changePopup] = useState(false)
+    const [display, changeDisplay] = useState({})
     useEffect(()=>{
         for(let doc of document.querySelectorAll('path')){
-            let result = energy.filter(obj => {
-                return (obj.bfs_nr_gemeinde == doc.id && obj.jahr==2020);
-            }).sort((a,b) => (a.jahr > b.jahr) ? -1 : ((b.jahr > a.jahr) ? 1 : 0))[0]
+            let result = props.energy.filter(obj => {
+                return ((obj.bfs_nr_gemeinde == doc.id && obj.jahr == "2020"));
+            })
             console.log(result)
             changeEnergy(oldArray => [...oldArray, result])
             doc.addEventListener('click', (e)=>{
                 changePopup(true);
+                changeDisplay(result[0])
+                console.log(energy)
             })
         }
         },[]
@@ -33,12 +36,28 @@ export default function Home(props) {
             <main>
                 <Map/>
                 <Popup changeTrigger={changePopup} trigger={popup}>
-
+                    <h3 className="text-2xl">{display.gemeinde_name?display.gemeinde_name:""}</h3>
                 </Popup>
             </main>
         </>
     )
 }
+
+/*
+* bfs_nr_gemeinde: "4911"
+biogasanlagen_abwasser: null
+biogasanlagen_industrie: null
+biogasanlagen_landwirtschaft: null
+biomasse_holz: null
+einwohner: 3946
+gemeinde_name: "Bürglen (TG)"
+jahr: "2020"
+kehricht: null
+photovoltaik: 1.88
+total: 15.108
+wasserkraft: 13.228
+wind: null
+* */
 
 export async function getStaticProps({params}) {
     const req = await fetch((await (await fetch('https://ckan.opendata.swiss/api/3/action/package_show?id=erneuerbare-elektrizitatsproduktion-nach-energietragern-und-gemeinden')).json()).result.resources.filter(obj=>{return obj.media_type=="application/json"})[0].uri);
