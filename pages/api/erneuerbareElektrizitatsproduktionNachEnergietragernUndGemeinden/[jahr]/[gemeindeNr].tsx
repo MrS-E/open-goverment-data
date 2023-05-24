8618d84ab1a2b13af2c38a3136255316c39c90dc
MrS-E <simeon.stix@gmx.ch>
2023-05-24 20:45:37 +0200

    add api point

---
+++ pages/api/erneuerbareElektrizitatsproduktionNachEnergietragernUndGemeinden/[jahr]/[gemeindeNr].tsx
@@ -0,0 +1,17 @@
+import {NextApiRequest, NextApiResponse} from "next";
+import {PrismaClient} from "@prisma/client";
+
+export default async function handler(req : NextApiRequest, res: NextApiResponse) {
+    const prisma:PrismaClient = new PrismaClient()
+    const jahr:string = req.query.jahr as string
+    const gNr: string = req.query.gemeindeNr as string
+    const data: any = await  prisma.erneuerbareElektrizitatsproduktionNachEnergietragernUndGemeinden.findUnique({
+        where:{
+            nr_gemeinde_jahr:{
+                nr_gemeinde: gNr,
+                jahr: jahr
+            }
+        }
+    })
+    res.status(200).send(data)
+}
